521ed567830305a65c03cddc5274124df23e50bb
kdbswo <75184363+kdbswo@users.noreply.github.com>
2022-06-04 06:34:59 +0900

    query

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 import { ApolloServer, gql } from "apollo-server";
 import { url } from "inspector";
 
+const typeDefs = gql`
+  type Query {
+    text: String
+    hello: String
+  }
+`;
 
-const server = new ApolloServer({})
+const server = new ApolloServer({ typeDefs });
 
 server.listen().then(({ url }) => {
   console.log(`Running on ${url}`);
