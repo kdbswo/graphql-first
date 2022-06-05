272287233890429ab6637ab31139e1db2daed940
kdbswo <75184363+kdbswo@users.noreply.github.com>
2022-06-06 07:22:32 +0900

    mutation

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ const typeDefs = gql`
     allTweets: [Tweet]
     tweet(id: ID): Tweet
   }
+  type Mutation {
+    postTweet(text: String, userId: ID): Tweet
+    deletTweet(id: ID): Boolean
+  }
 `;
 
 const server = new ApolloServer({ typeDefs });
